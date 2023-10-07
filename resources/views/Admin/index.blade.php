4eccc734a8ac2fc5b1c8e5e631c8be2b79d2a19a
juan21012001 <juanjoseorozco9@gmail.com>
2023-10-07 11:45:45 -0500

    first commit

---
+++ resources/views/Admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminPT')
+
+@section('content_header')
+    <h1><span class="pf pf-atm"> Popayan Turims</span></h1>
+@stop
+
+@section('content')
+    <p class="h5">Bienvenido a Popayan Turims</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
